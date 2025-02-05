3aeb786678c3fd76c07c9c5760ee3cc2cccde1b0
takechi-scratch <takechi.scratch@gmail.com>
2025-02-05 22:25:25 +0900

    fix suspendバグ修正

--- src/app/questions/page.jsx
+++ src/app/questions/page.jsx
@@ -13,6 +13,11 @@ const Questions = () => {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [showAnswer, setShowAnswer] = useState(false);
 
+    const refresh = () => {
+        fetchQuestions(currentDatabaseID, blockID, false);
+        router.refresh();
+    };
+
     useEffect(() => {
         fetchQuestions(currentDatabaseID, blockID).then((data) => {
             setQuestions(data);
@@ -44,32 +49,26 @@ const Questions = () => {
     const currentQuestion = questions[currentQuestionIndex];
 
     return (
-        <div className="p-4 bg-white rounded shadow-md">
-            <p className="text-lg font-semibold mb-2">ID: {currentQuestion.id}</p>
-            <div className="cursor-pointer mb-4 min-h-24 lg:min-h-8">
-                <p className="text-xl">{currentQuestion.question}</p>
+        <>
+            <div className="p-4 bg-white rounded shadow-md">
+                <p className="text-lg font-semibold mb-2">ID: {currentQuestion.id}</p>
+                <div className="cursor-pointer mb-4 min-h-24 lg:min-h-8">
+                    <p className="text-xl">{currentQuestion.question}</p>
+                </div>
+                <button className="p-4 border border-gray-300 rounded-md text-left hover:bg-gray-50 w-full" onClick={handleToggleAnswer}>
+                    <p className={`${showAnswer && "text-green-600"} p-1`}>{showAnswer ? currentQuestion.answer : "答えを表示"}</p>
+                </button>
+                <div className="flex justify-between space-x-4 mt-4">
+                    <button className="px-8 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" onClick={handlePrevQuestion}>前の問題</button>
+                    <button className="px-8 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" onClick={handleNextQuestion}>次の問題</button>
+                </div>
             </div>
-            <button className="p-4 border border-gray-300 rounded-md text-left hover:bg-gray-50 w-full" onClick={handleToggleAnswer}>
-                <p className={`${showAnswer && "text-green-600"} p-1`}>{showAnswer ? currentQuestion.answer : "答えを表示"}</p>
-            </button>
-            <div className="flex justify-between space-x-4 mt-4">
-                <button className="px-8 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" onClick={handlePrevQuestion}>前の問題</button>
-                <button className="px-8 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" onClick={handleNextQuestion}>次の問題</button>
-            </div>
-        </div>
+            <button className="px-8 py-2 bg-gray-500 text-white rounded hover:bg-gray-700 self-start" onClick={() => window.history.back()}>戻る</button>
+        </>
     );
 }
 
 export default function Main() {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const blockID = searchParams.get("blockID");
-
-    const refresh = () => {
-        fetchQuestions(currentDatabaseID, blockID, false);
-        router.refresh();
-    };
-
     return (
         <div className="flex flex-col min-h-screen lg:px-8">
             <Header />
@@ -78,7 +77,6 @@ export default function Main() {
                 <Suspense fallback={<div>読み込み中...</div>}>
                     <Questions />
                 </Suspense>
-                <button className="px-8 py-2 bg-gray-500 text-white rounded hover:bg-gray-700 self-start" onClick={() => window.history.back()}>戻る</button>
                 <button className="px-8 py-2 bg-red-500 text-white rounded hover:bg-red-700 self-start" onClick={() => refresh()}>キャッシュなしで再読み込み</button>
             </main>
         </div>
