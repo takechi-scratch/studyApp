4104db56cc2d6c70972ef459203be3ebe22a6e1e
takechi-scratch <takechi.scratch@gmail.com>
2025-02-06 21:01:03 +0900

    add Googleアナリティクス

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from 'next/script';
 
 export const metadata = {
     title: "テスト対策アプリ",
@@ -6,6 +7,8 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+    const gaId = "G-9EM0X7ZXLQ";
+
     return (
         <html lang="ja">
             <head>
@@ -13,6 +16,21 @@ export default function RootLayout({ children }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>{metadata.title}</title>
                 <meta name="description" content={metadata.description} />
+
+                <Script
+                    src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+                    strategy='afterInteractive'
+                    async
+                />
+                <Script id='google-analytics' strategy='afterInteractive'>
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${gaId}');
+                `}
+                </Script>
+
             </head>
             <body>
                 {children}
