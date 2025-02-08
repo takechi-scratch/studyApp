995e2f53296e347ce9781917d2ad691b2e64e0ce
takechi-scratch <takechi.scratch@gmail.com>
2025-02-08 20:54:49 +0900

    add 設定（バグあり）

--- src/app/questions/page.jsx
+++ src/app/questions/page.jsx
@@ -1,33 +1,15 @@
 "use client";
 
-import { useState, useEffect, Suspense } from "react";
-import { useSearchParams } from "next/navigation";
+import { useState, useEffect, Suspense, useRef, use } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
 
 import Header from "../../components/header";
 import { currentDatabaseID, fetchQuestions } from "@/features/questionsData";
 import Message from "@/components/message";
 
-const Questions = () => {
-    const searchParams = useSearchParams();
-    const blockID = searchParams.get("blockID");
-
-    const [questions, setQuestions] = useState(null);
+const Questions = ({questions, isRandom, includesFeedback}) => {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [showAnswer, setShowAnswer] = useState(false);
-    const [message, setMessage] = useState("");
-
-    const refresh = () => {
-        fetchQuestions(currentDatabaseID, blockID, false).then((data) => {
-            setQuestions(data);
-            setMessage("データを再読み込みしました！"); // 一度しか出てこない
-        });
-    };
-
-    useEffect(() => {
-        fetchQuestions(currentDatabaseID, blockID).then((data) => {
-            setQuestions(data);
-        });
-    }, [blockID]);
 
     const handleToggleAnswer = () => {
         setShowAnswer(!showAnswer);
@@ -43,15 +25,16 @@ const Questions = () => {
         setShowAnswer(false);
     };
 
-    if (!questions) {
-        return <p>読み込み中...</p>;
-    }
-
-    if (questions.length === 0) {
-        return <p className="text-xl">問題がありません。</p>;
-    }
-
     const currentQuestion = questions[currentQuestionIndex];
+
+    let answer = "答えを表示";
+    if (showAnswer) {
+        if (includesFeedback) {
+            answer = `${currentQuestion.answer}\n${currentQuestion.feedback}`;
+        } else {
+            answer = currentQuestion.answer;
+        }
+    }
 
     return (
         <>
@@ -61,15 +44,96 @@ const Questions = () => {
                     <p className="text-xl">{currentQuestion.question}</p>
                 </div>
                 <button className="p-4 border border-gray-300 rounded-md text-left hover:bg-gray-50 w-full" onClick={handleToggleAnswer}>
-                    <p className={`${showAnswer && "text-green-600"} p-1`}>{showAnswer ? currentQuestion.answer : "答えを表示"}</p>
+                    <p className={`${showAnswer && "text-green-600"} p-1`}>{answer}</p>
                 </button>
                 <div className="flex justify-between space-x-4 mt-4">
                     <button className="px-8 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" onClick={handlePrevQuestion}>前の問題</button>
                     <button className="px-8 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" onClick={handleNextQuestion}>次の問題</button>
                 </div>
             </div>
-            <button className="px-8 py-2 bg-red-500 text-white rounded hover:bg-red-700 self-start" onClick={() => refresh()}>キャッシュなしで再読み込み</button>
-            {message && <Message text={message} className="bg-green-300" />}
+        </>
+    );
+}
+
+const Settings = ({isRandom, setIsRandom, includesFeedback, setIncludesFeedback}) => {
+    const [message, setMessage] = useState("");
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const refresh = () => {
+        fetchQuestions(currentDatabaseID, blockID, false).then((data) => {
+            setQuestions(data);
+            setMessage("データを再読み込みしました！"); // 一度しか出てこないバグあり
+        });
+    };
+
+    const handleToggleRandom = () => {
+        setIsRandom(!isRandom);
+    };
+
+    const handleToggleFeedback = () => {
+        setIncludesFeedback(!includesFeedback);
+    };
+
+    return (
+        <div className="relative">
+            <button className="p-4 border border-gray-300 rounded-md text-left hover:bg-gray-50 w-full" onClick={toggleMenu}>
+                設定
+            </button>
+            {isOpen && (
+                <div className="absolute mt-2 w-48 bg-white border border-gray-300 rounded shadow-lg">
+                    <ul>
+                        <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                            <label>
+                                <input type="checkbox" checked={isRandom} onChange={handleToggleRandom} />
+                                ランダム
+                            </label>
+                        </li>
+                        <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                            <label>
+                                <input type="checkbox" checked={includesFeedback} onChange={handleToggleFeedback} />
+                                フィードバックを含む
+                            </label>
+                        </li>
+                    </ul>
+                    <button className="px-8 py-2 bg-red-500 text-white rounded hover:bg-red-700 self-start" onClick={() => refresh()}>キャッシュなしで再読み込み</button>
+                    {message && <Message text={message} className="bg-green-300" />}
+                </div>
+            )}
+        </div>
+    );
+}
+
+
+const Root = () => {
+    const [questions, setQuestions] = useState(null);
+    const [isRandom, setIsRandom] = useState(false);
+    const [includesFeedback, setIncludesFeedback] = useState(false);
+
+    const searchParams = useSearchParams();
+    const blockID = searchParams.get("blockID");
+
+    useEffect(() => {
+        fetchQuestions(currentDatabaseID, blockID).then((data) => {
+            setQuestions(data);
+        });
+    }, [blockID]);
+
+    if (!questions) {
+        return <p>読み込み中...</p>;
+    }
+
+    if (questions.length === 0) {
+        return <p>問題がありません。</p>;
+    }
+
+    return (
+        <>
+            <Questions questions={questions} isRandom={isRandom} includesFeedback={includesFeedback} />
+            <Settings isRandom={isRandom} setIsRandom={setIsRandom} includesFeedback={includesFeedback} setIncludesFeedback={setIncludesFeedback} />
         </>
     );
 }
@@ -81,7 +145,7 @@ export default function Main() {
             <main className="flex flex-col justify-between p-8 lg:p-24 gap-8">
                 <h1 className="text-4xl font-bold">問題</h1>
                 <Suspense fallback={<div>読み込み中...</div>}>
-                    <Questions />
+                    <Root />
                 </Suspense>
                 <button className="px-8 py-2 bg-gray-500 text-white rounded hover:bg-gray-700 self-start" onClick={() => window.history.back()}>戻る</button>
             </main>
